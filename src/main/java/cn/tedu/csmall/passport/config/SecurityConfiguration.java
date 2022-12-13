eeff48e9459e851356cc9a74c13f831fb071b4d6
成恒 <chengheng@tedu.cn>
2022-12-13 11:15:20 +0800

    .

--- src/main/java/cn/tedu/csmall/passport/config/SecurityConfiguration.java
+++ src/main/java/cn/tedu/csmall/passport/config/SecurityConfiguration.java
@@ -17,8 +17,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // super.configure(http); // 父类方法配置要求所有请求都是认证过的
 
+        // 白名单URL
+        // 注意：所有路径使用 / 作为第1个字符
+        // 可以使用 * 通配符，例如 /admins/* 可以匹配 /admins/add-new，但是，不能匹配多级，例如不能匹配到 /admins/9527/delete
+        // 可以使用 ** 通配符，例如 /admins/** 可以匹配若干级，例如可以匹配 /admins/add-new，也可以匹配到 /admins/9527/delete
+        String[] urls = {
+                "/doc.html",
+                "/**/*.css",
+                "/**/*.js",
+                "/favicon.ico",
+                "/swagger-resources",
+                "/v2/api-docs"
+        };
+
+        // 配置请求是否需要认证
         http.authorizeRequests() // 配置请求的认证授权
-                .mvcMatchers("/doc.html", "", "") // 匹配某些请求路径
+                .mvcMatchers(urls) // 匹配某些请求路径
                 .permitAll() // 允许直接访问，即不需要通过认证
                 .anyRequest() // 其它任何请求
                 .authenticated(); // 需要是通过认证的

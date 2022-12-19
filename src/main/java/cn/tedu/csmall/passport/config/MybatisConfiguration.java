134e4be7944dbcb2ac7fa3e742c68c62a90aa0e6
成恒 <chengheng@tedu.cn>
2022-12-19 21:07:42 +0800

    .

--- src/main/java/cn/tedu/csmall/passport/config/MybatisConfiguration.java
+++ src/main/java/cn/tedu/csmall/passport/config/MybatisConfiguration.java
@@ -1,8 +1,14 @@
 package cn.tedu.csmall.passport.config;
 
+import cn.tedu.csmall.passport.mybatis.InsertUpdateTimeInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * Mybatis配置类
@@ -17,6 +23,17 @@ public class MybatisConfiguration {
 
     public MybatisConfiguration() {
         log.debug("创建配置类对象：MybatisConfiguration");
+    }
+
+    @Autowired
+    private List<SqlSessionFactory> sqlSessionFactoryList;
+
+    @PostConstruct // 在方法上添加@PostConstruct注解，表示此方法是Spring Bean的生命周期方法的初始化方法，会在创建对象之后自动执行
+    public void addInterceptor() {
+        InsertUpdateTimeInterceptor interceptor = new InsertUpdateTimeInterceptor();
+        for (SqlSessionFactory sqlSessionFactory : sqlSessionFactoryList) {
+            sqlSessionFactory.getConfiguration().addInterceptor(interceptor);
+        }
     }
 
 }
